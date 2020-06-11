8c4acab1c271366a28ba348388b3695aa5c27a62
Dhanush Adithya <59655416+BDA1211@users.noreply.github.com>
2020-06-11 13:11:21 +0530

    Delete event.js

--- event.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose"),
-	passportLocalMongoose = require("passport-local-mongoose");;
-
-var eventschema = new mongoose.Schema({
-	Name : String,
-	Date : String,
-	id   : String
-});
-module.exports = mongoose.model("Event", eventschema);
