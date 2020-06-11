4fe21469377ccbde025b023da4ef5fb31af0a7ce
Dhanush Adithya <59655416+BDA1211@users.noreply.github.com>
2020-06-11 13:19:42 +0530

    Delete user.js

--- user.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose"),
-	passportLocalMongoose = require("passport-local-mongoose");
-var userschema = new mongoose.Schema({
-	username : String,
-	password : String,
-});
-userschema.plugin(passportLocalMongoose);
-module.exports = mongoose.model("User", userschema);
